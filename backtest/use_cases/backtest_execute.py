26191f8eec87285a7af1e3938ada4aee6e3858ac
Moosong - macbook <moosong@macbook.com>
2023-02-05 18:14:42 +0900

    todo: test backtest execute

--- backtest/use_cases/backtest_execute.py
+++ backtest/use_cases/backtest_execute.py
@@ -1,10 +1,31 @@
 from backtest.domains.backtest import Backtest
 from backtest.domains.backtest_result import BacktestResult
+from backtest.domains.strategy import Strategy
+from backtest.domains.strategy import StockData
+from backtest.domains.strategy_result import StrategyResultColumnType
+from backtest.use_cases.strategy_execute import strategy_execute
 from backtest.response import ResponseFailure, ResponseSuccess, ResponseTypes
+
+import pandas as pd
 
 
 def backtest_execute(backtest: Backtest):
     try:
+        for stockdata in backtest.stockdata_list:
+            strategy_total_result = pd.DataFrame(index=pd.DatetimeIndex([]))
+            for strategy in backtest.strategy_list:
+                response = strategy_execute(strategy=strategy)
+                if isinstance(response, ResponseFailure):
+                    raise Exception('strategy_response error!')
+                else:
+                    strategy_result = response.value
+                    if strategy_result.target == stockdata.symbol or strategy_result.target == 'ALL':
+                        strategy_total_result = strategy_total_result.join(
+                            strategy_result.value, how='outer')
+            for column in strategy_total_result.columns:
+                strategy_total_result[column] = strategy_total_result[column].fillna(
+                    {i: (StrategyResultColumnType.KEEP, 0) for i in strategy_total_result.index})
+
         return ResponseSuccess(BacktestResult())
     except Exception as e:
         return ResponseFailure(ResponseTypes.SYSTEM_ERROR, e)
--- tests/use_cases/test_backtest_execute.py
+++ tests/use_cases/test_backtest_execute.py
@@ -3,34 +3,83 @@ from backtest.domains.backtest import Backtest
 from backtest.domains.strategy import Strategy
 from backtest.domains.stockdata import StockData
 from backtest.domains.backtest_result import BacktestResult
+from backtest.domains.strategy_result import StrategyResult, StrategyResultColumnType
 from backtest.response import ResponseSuccess
 import pytest
+import pandas as pd
+from unittest import mock
+
+
+@pytest.fixture(scope='function')
+def dict_stock_data_list():
+    return [{'open': [0.00000, '1.11111', '3.3333', '0.0000', '1.0000'],
+            'high': [0.00000, '1.11111', '3.3333', '0.0000', '1.0000'],
+             'low': [0.00000, '1.11111', '3.3333', '0.0000', '1.0000'],
+             'close': [0.00000, '1.11111', '3.3333', '0.0000', '1.0000'],
+             'volume': [0.00000, '1.11111', '3.3333', '0.0000', '1.0000'],
+             'date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05']},
+            {'open': [0.00000, '1.11111', '2.2222', '0.0000', '1.0000'],
+            'high': [0.00000, '1.11111', '2.2222', '0.0000', '1.0000'],
+             'low': [0.00000, '1.11111', '2.2222', '0.0000', '1.0000'],
+             'close': [0.00000, '1.11111', '2.2222', '0.0000', '1.0000'],
+             'volume': [0.00000, '1.11111', '2.2222', '0.0000', '1.0000'],
+             'date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05']},
+            {'open': [0.00000, '1.11111', '3.3333', '0.0000', '1.0000'],
+            'high': [0.00000, '1.11111', '4.4444', '0.0000', '1.0000'],
+             'low': [0.00000, '1.11111', '4.4444', '0.0000', '1.0000'],
+             'close': [0.00000, '1.11111', '4.4444', '0.0000', '1.0000'],
+             'volume': [0.00000, '1.11111', '5.5555', '0.0000', '1.0000'],
+             'date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05']}]
 
 
 @pytest.fixture(scope='function')
 def strategy_list():
-    strategy1 = Strategy()
-    strategy2 = Strategy()
-    strategy3 = Strategy()
+    strategy1 = Strategy(name='test1')
+    strategy2 = Strategy(name='test2')
+    strategy3 = Strategy(name='test3')
     return [
         strategy1, strategy2, strategy3
     ]
 
 
 @pytest.fixture(scope='function')
-def stockdata_list():
-    stockdata1 = StockData()
-    stockdata2 = StockData()
-    stockdata3 = StockData()
+def stockdata_list(dict_stock_data_list):
+    stockdata1 = StockData.from_dict(
+        dict_data=dict_stock_data_list[0], symbol='teststock1')
+    stockdata2 = StockData.from_dict(
+        dict_data=dict_stock_data_list[1], symbol='teststock2')
+    stockdata3 = StockData.from_dict(
+        dict_data=dict_stock_data_list[2], symbol='teststock3')
     return [
         stockdata1, stockdata2, stockdata3
     ]
 
 
-def test_backtest_execute_without_options(strategy_list, stockdata_list):
+@pytest.fixture(scope='function')
+def stockdata_result():
+    sample_dict = {'date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05'],
+                   'strategy1': [(StrategyResultColumnType.KEEP, 100),
+                                 (StrategyResultColumnType.BUY, 100),
+                                 (StrategyResultColumnType.SELL, 100),
+                                 (StrategyResultColumnType.SELL, 100),
+                                 (StrategyResultColumnType.BUY, 100),]}
+    return StrategyResult.from_dict(sample_dict, target='ALL')
+
+
+@mock.patch('backtest.use_cases.backtest_execute.strategy_execute')
+def test_backtest_execute_without_options(strategy_execute, strategy_list, stockdata_list, stockdata_result):
+    strategy_execute.return_value = ResponseSuccess(stockdata_result)
     strategies = strategy_list
     stockdata = stockdata_list
     backtest = Backtest(strategy_list=strategies, stockdata_list=stockdata)
     response = backtest_execute(backtest)
+    strategy_execute.assert_called()
     assert isinstance(response.value, BacktestResult)
     assert bool(response) == True
+
+    backtest_result = response.value
+    assert isinstance(backtest_result.value, pd.DataFrame)
+    assert backtest_result.value.index[0] == '2022-01-01'
+    assert isinstance(backtest_result.value.index, pd.DatetimeIndex)
+    assert list(backtest_result.value.columns) == ['stock_bucket',
+                                                   'total_profit']
