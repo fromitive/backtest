19c6273825e99e6d6c2122ae530a1cadc4890819
Moosong - macbook <moosong@macbook.com>
2023-02-22 05:21:36 +0900

    add strategy function (rsi, rsi_big_stock)

--- backtest/use_cases/strategy_execute.py
+++ backtest/use_cases/strategy_execute.py
@@ -13,6 +13,42 @@ def basic_function(data: StockData, weight: int, name: str):
     return response
 
 
+def _dataframe_rsi(df: pd.DataFrame, weight: int, rolling=100):
+    df['sma'] = df['close'].rolling(rolling).mean().fillna(0)
+    df['smashift'] = df['sma'].shift(1).fillna(0)
+
+    def _rsi_internal(r):
+        if (r.smashift - r.sma) > 0.0:
+            return (StrategyResultColumnType.SELL, weight)
+        elif (r.smashift - r.sma) == 0.0:
+            return (StrategyResultColumnType.KEEP, weight)
+        else:
+            return (StrategyResultColumnType.BUY, weight)
+    df['result'] = df.apply(lambda r: _rsi_internal(r), axis=1)
+    return df
+
+
+def rsi_function(data: StockData, weight: int, name: str, rolling=100):
+    response = StrategyResult(value=pd.DataFrame(
+        index=data.data.index, columns=[name]))
+    """
+    strategyfunction here
+    """
+    response.value[name] = _dataframe_rsi(data.data, weight, rolling)['result']
+    return response
+
+
+def rsi_big_stock_function(data: StockData, weight: int, name: str, big_stock: StockData, rolling=100):
+    response = StrategyResult(value=pd.DataFrame(
+        index=data.data.index, columns=[name]))
+    """
+    strategyfunction here
+    """
+    response.value[name] = _dataframe_rsi(
+        big_stock.data, weight, rolling)['result']
+    return response
+
+
 def strategy_execute(strategy: Strategy, data: StockData):
     try:
         if not strategy.function:
