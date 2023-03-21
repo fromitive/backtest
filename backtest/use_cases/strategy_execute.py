7fd19bce6245f8ff580d80d990609af07c86233c
wonhee <dldnjs663@gmail.com>
2023-03-21 22:18:03 +0900

    rsi function added (not meaning.. :( )

--- backtest/use_cases/strategy_execute.py
+++ backtest/use_cases/strategy_execute.py
@@ -14,40 +14,82 @@ def basic_function(data: StockData, weight: int, name: str):
     return response
 
 
-def _dataframe_rsi(df: pd.DataFrame, weight: int, rolling=100):
+
+def _dataframe_sma(df: pd.DataFrame, weight: int, rolling=100):
     df['sma'] = df['close'].rolling(rolling).mean().fillna(0)
     df['smashift'] = df['sma'].shift(1).fillna(0)
 
-    def _rsi_internal(r):
+    def _sma_internal(r):
         if (r.smashift - r.sma) > 0.0:
             return (StrategyResultColumnType.SELL, weight)
         elif (r.smashift - r.sma) == 0.0:
             return (StrategyResultColumnType.KEEP, weight)
         else:
             return (StrategyResultColumnType.BUY, weight)
-    df['result'] = df.apply(lambda r: _rsi_internal(r), axis=1)
+    df['result'] = df.apply(lambda r: _sma_internal(r), axis=1)
     return df
 
 
-def rsi_function(data: StockData, weight: int, name: str, rolling=100):
+def sma_function(data: StockData, weight: int, name: str, rolling=100):
     response = StrategyResult(value=pd.DataFrame(
         index=data.data.index, columns=[name]))
     """
     strategyfunction here
     """
-    response.value[name] = _dataframe_rsi(data.data, weight, rolling)['result']
+    response.value[name] = _dataframe_sma(data.data, weight, rolling)['result']
     return response
 
 
-def rsi_big_stock_function(data: StockData, weight: int, name: str, big_stock: StockData, rolling=100):
+def sma_big_stock_function(data: StockData, weight: int, name: str, big_stock: StockData, rolling=100):
     response = StrategyResult(value=pd.DataFrame(
         index=data.data.index, columns=[name]))
     """
     strategyfunction here
     """
-    response.value[name] = _dataframe_rsi(
+    response.value[name] = _dataframe_sma(
         big_stock.data, weight, rolling)['result']
     return response
+
+def _calculate_rsi(data, period):
+    delta = data.diff().dropna()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.rolling(window=period).mean()
+    avg_loss = loss.rolling(window=period).mean()
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def rsi_function(data: StockData, weight: int, name: str, period: int,overbought_level: int,oversold_level: int):
+    response = StrategyResult(value=pd.DataFrame(
+    index=data.data.index, columns=[name]))
+    data.data['rsi']=_calculate_rsi(data.data['close'],period)
+    def _rsi_function(r):
+        if r <= oversold_level and r > oversold_level:
+            return (StrategyResultColumnType.BUY, weight)
+        elif r >= overbought_level and r < overbought_level:
+            return (StrategyResultColumnType.SELL, weight)
+        else:
+            return (StrategyResultColumnType.KEEP, weight) 
+    response.value[name]=data.data.apply(lambda r:_rsi_function(r['close']),axis=1)
+    return response
+
+def rsi_big_stock_function(data: StockData, weight: int, name: str, big_stock: StockData,period: int,overbought_level: int,oversold_level: int):
+    response = StrategyResult(value=pd.DataFrame(
+    index=data.data.index, columns=[name]))
+    data.data['rsi']=_calculate_rsi(big_stock.data['close'],period)
+    def _rsi_function(r):
+        if r <= oversold_level and r > oversold_level:
+            return (StrategyResultColumnType.BUY, weight)
+        elif r >= overbought_level and r < overbought_level:
+            return (StrategyResultColumnType.SELL, weight)
+        else:
+            return (StrategyResultColumnType.KEEP, weight) 
+    response.value[name]=data.data.apply(lambda r:_rsi_function(r['close']),axis=1)
+    return response
+
 
 def greed_fear_index_function(data: StockData, weight: int, name: str, greed_fear_index_data: pd.DataFrame,index_fear: int,index_greed: int):
     response = StrategyResult(value=pd.DataFrame(
