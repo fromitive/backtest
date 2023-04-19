6a486c1da5a0084a34a448c04ee5e5b2013167ab
Moosong - macbook <moosong@macbook.com>
2023-04-19 21:41:04 +0900

    cache test added

--- tests/use_cases/test_selector_reference_from_repo.py
+++ tests/use_cases/test_selector_reference_from_repo.py
@@ -1,17 +1,21 @@
-import pytest
 from unittest import mock
+
+import pytest
+
 from backtest.domains.selector_reference import SelectorReference
-from backtest.request.selector_reference_from_repo import build_selector_reference_from_repo_request
-from backtest.use_cases.selector_reference_from_repo import selector_reference_from_repo
+from backtest.request.selector_reference_from_repo import \
+    build_selector_reference_from_repo_request
 from backtest.response import ResponseFailure
+from backtest.use_cases.selector_reference_from_repo import \
+    selector_reference_from_repo
 
 
 @pytest.fixture(scope='function')
 def sample_empty_selector_reference():
-    return SelectorReference()
+    return SelectorReference(symbol='TEST')
 
 
-def test_stockdata_from_repo(sample_empty_selector_reference):
+def test_selector_reference_from_repo(sample_empty_selector_reference):
     repo = mock.Mock()
     repo.get.return_value = sample_empty_selector_reference
     request = build_selector_reference_from_repo_request()
@@ -21,7 +25,17 @@ def test_stockdata_from_repo(sample_empty_selector_reference):
     assert response.value == sample_empty_selector_reference
 
 
-def test_stockdata_from_invalied_request_repo(sample_empty_selector_reference):
+def test_selector_reference_from_repo_with_cache_enable(sample_empty_selector_reference):
+    repo = mock.Mock()
+    repo.get.return_value = sample_empty_selector_reference
+    request = build_selector_reference_from_repo_request()
+    response = selector_reference_from_repo(repo, request, cache=True)
+    repo.get.assert_called_with(filters=None)
+    assert bool(response) == True
+    assert response.value == sample_empty_selector_reference
+
+
+def test_selector_reference_from_invalied_request_repo(sample_empty_selector_reference):
     repo = mock.Mock()
     repo.get.return_value = sample_empty_selector_reference
     request = None
