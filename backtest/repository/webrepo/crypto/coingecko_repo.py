a040ba24863d52742d926b3c4cc39cb1b848713b
무송 이 <ms.lee@oegroup.co.kr>
2023-04-19 14:52:10 +0900

    coingecko_repo added

---
+++ backtest/repository/webrepo/crypto/coingecko_repo.py
@@ -0,0 +1,61 @@
+# coingeckco repo - return SelectorReference
+import time
+from datetime import datetime
+
+import pandas as pd
+import requests
+
+from backtest.domains.selector_reference import SelectorReference
+from backtest.util.selector_reference_util import get_coingecko_symbol_id
+
+
+def convert_timestamp(str_date: str) -> int:
+    return int(datetime.strptime(str_date, "%Y-%m-%d").timestamp())
+
+
+class CoinGeckoRepo:
+    API_URL = 'https://www.coingecko.com/market_cap/{symbol_id}/usd/custom.json?from={from_timestamp}&to={from_timestamp}'
+    API_HEADERS = {"accept": "application/json"}
+
+    def __init__(self):
+        self.symbol = 'BTC'
+        self.from_date = '1999-01-01'
+        self.to_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+
+        self.from_timestamp = convert_timestamp('1999-01-01')
+        self.to_timestamp = int(datetime.now().timestamp())
+        self.symbol_id = '1'
+
+    def get(self, filters=None):
+        if filters:
+            filter = list(filters.keys())
+            self.symbol = filters['symbol__eq'] if 'symbol__eq' in filter else 'BTC'
+            self.from_date = filters['from__eq'] if 'from__eq' in filter else '1999-01-01'
+            self.to_date = filters['from__eq'] if 'to__eq' in filter else datetime.strftime(
+                datetime.now(), "%Y-%m-%d")
+            self.from_timestamp = convert_timestamp(self.from_date)
+            self.to_timestamp = convert_timestamp(self.to_date)
+            self.symbol_id = get_coingecko_symbol_id(self.symbol)
+        request_url = self.API_URL.format(
+            symbol_id=self.symbol_id,
+            from_timestamp=self.from_timestamp,
+            to_timestamp=self.to_timestamp)
+        response = requests.get(request_url, headers=self.API_HEADERS)
+        if response.status_code == 200:
+            dict_data = response.json().get('data')['stats']
+            temp_df = pd.DataFrame(
+                dict_data, columns=['time', 'marketcap'])
+            temp_df['date'] = temp_df['time'].apply(
+                lambda x: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(x / 1000)))
+            temp_df.set_index('date', inplace=True)
+            temp_df.sort_index(ascending=True, inplace=True)
+            temp_df['date'] = temp_df.index
+            print(temp_df)
+            if self.from_date:
+                temp_df = temp_df.loc[self.from_date:]
+            if self.to_date:
+                temp_df = temp_df.loc[:self.to_date]
+            temp_df = temp_df[['date', 'marketcap']]
+            return SelectorReference.from_dict(temp_df.to_dict('list'), self.symbol)
+        else:
+            raise Exception('request error', response.status_code)
