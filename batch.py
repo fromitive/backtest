581bfc72e1c3d88e85f6841959ff2e50c6e7f9d3
무송 이 <ms.lee@oegroup.co.kr>
2023-04-21 17:02:01 +0900

    batch update - request module move (use_cases)

--- batch.py
+++ batch.py
@@ -1,13 +1,10 @@
-import requests
-
+from backtest.use_cases.make_crypto_backtest import (get_bithumb_symbol,
+                                                     get_upbit_symbol)
 from backtest.util.selector_reference_util import get_coingecko_symbol_id
 
 
 def batch_bithumb_symbol_to_geckco_id():
-    URL_BITUMB_SYMBOL_API = 'https://api.bithumb.com/public/ticker/ALL_KRW'
-    res = requests.get(URL_BITUMB_SYMBOL_API).json().get('data')
-
-    symbol_list = list(res.keys())[:-1]
+    symbol_list = get_bithumb_symbol()
     symbol_list_len = len(symbol_list)
 
     for idx, symbol in enumerate(symbol_list, start=1):
@@ -17,10 +14,7 @@ def batch_bithumb_symbol_to_geckco_id():
 
 
 def batch_upbit_symbol_to_geckco_id():
-    URL_UPBIT_SYMBOL_API = 'https://api.upbit.com/v1/market/all'
-    res = requests.get(URL_UPBIT_SYMBOL_API).json()
-    symbol_list = [symbol['market'][4:]
-                   for symbol in res if symbol['market'].split('-')[0] == 'KRW']
+    symbol_list = get_upbit_symbol()
     symbol_list_len = len(symbol_list)
 
     for idx, symbol in enumerate(symbol_list, start=1):
