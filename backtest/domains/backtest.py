6addd67016de464ab60ba3f73e8d1d78b4962e4b
무송 이 <ms.lee@oegroup.co.kr>
2023-04-26 14:52:13 +0900

    add selector_result for selector

--- backtest/domains/backtest.py
+++ backtest/domains/backtest.py
@@ -1,6 +1,7 @@
 import dataclasses
 from typing import List
 
+from backtest.domains.selector_result import SelectorResult
 from backtest.domains.stockdata import StockData
 from backtest.domains.strategy import Strategy
 
@@ -9,4 +10,5 @@ from backtest.domains.strategy import Strategy
 class Backtest:
     strategy_list: List[Strategy]
     stockdata_list: List[StockData]
+    selector_result: SelectorResult = None
     buy_price: float = 5000.0
